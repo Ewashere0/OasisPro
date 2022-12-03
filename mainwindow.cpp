b4b47e91d6b22561163b22308a8bbc8471906f43
Ricky Gulati <rickygulati25@gmail.com>
2022-12-02 23:49:34 -0500

    Skeleton code for mainwindow
    Added function headers, skeleton functions to mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,6 +25,109 @@ void MainWindow::greenLightOff(){
     ui->pwrLight->repaint();
 }
 
+void turnOn() {
+    // DO SOMETHING
+}
+void turnOff() {
+    // DO SOMETHING
+}
+
+void promptSignIn() {
+    // DO SOMETHING
+}
+
+void standby() {
+    // DO SOMETHING
+}
+
+void handlePowerPress() {
+    // DO SOMETHING
+}
+
+void updateSessionsMenu() {
+    // DO SOMETHING
+}
+
+void handleDownPress() {
+    // DO SOMETHING
+}
+
+void handleUpPress() {
+    // DO SOMETHING
+}
+
+void handleSelectPress() {
+    // DO SOMETHING
+}
+
+void startSession() {
+    // DO SOMETHING
+}
+
+void displayBatteryLevel() {
+    // DO SOMETHING
+}
+
+void updateBatteryLevel() {
+    // DO SOMETHING
+}
+
+void handleBatteryLow() {
+    // DO SOMETHING
+}
+
+void startConnectionTest() {
+    // DO SOMETHING
+}
+
+void displayConnection() {
+    // DO SOMETHING
+}
+
+void connectionInvalid() {
+    // DO SOMETHING
+}
+
+void connectionValid() {
+    // DO SOMETHING
+}
+
+void endConnectionTest() {
+    // DO SOMETHING
+}
+
+void continueSession() {
+    // DO SOMETHING
+}
+
+void updateIntensity() {
+    // DO SOMETHING
+}
+
+void savePreferences() {
+    // DO SOMETHING
+}
+
+void promptToRecord() {
+    // DO SOMETHING
+}
+
+void recordTherapy() {
+    // DO SOMETHING
+}
+
+void createUser() {
+    // DO SOMETHING
+}
+
+void selectUser() {
+    // DO SOMETHING
+}
+
+void signIn() {
+    // DO SOMETHING
+}
+
 /* SLOTS */
 void MainWindow::on_pwrButton_clicked()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -29,10 +29,49 @@ public:
     
     
 private:
+
+    int powerStatus;    // indicator of power status
+    bool inSessionStatus;   // checks whether a session is ongoing
+    int currentSessionGroup;   // indicates what session group (duration) is selected
+    int currentSessionType; // indicates what session type the user chose
+    bool signedInStatus;    // checks whether user is signed in
+    int batteryLevel;   // indicates the battery level
+    int currentIntensity; // indicates the intensity level
+
     /* Some helper functions that can be cut and paste later into functions to do some operations on the UI
      * Not sure about what conditions need to be satisfied to do a lot of these so I am putting them as helpers for now
      */
     
+    void turnOn(); // turns on the device by enabling the UI
+    void turnOff(); // turns off the device by disabling the UI
+
+    void promptSignIn(); // opens the sign in prompt for the user
+    void standby(); // what does this function do?
+
+    void handlePowerPress(); // handles the functionality for the power button
+    void updateSessionsMenu(); // updates the UI of the available sessions depending on the session group
+    void handleDownPress(); // handles the down button press for selecting session type
+    void handleUpPress(); // handles the up button press for selecting session type
+    void handleSelectPress(); // handles the select button press to confirm the session start
+    void startSession(); // starts the session functionality
+    void displayBatteryLevel(); // displays the current battery level by checking the batteryLevel variable
+    void updateBatteryLevel(); // updates the batterylevel variable after
+    void handleBatteryLow(); // handles the battery level low extension
+    void startConnectionTest(); // to check device connection before session start
+    void displayConnection(); // displays connection strength on UI (on the numbers 1-4 bar)
+    void connectionInvalid(); // handles the case where connection status is low
+    void connectionValid(); // confirms that connection is valid so session can start
+    void endConnectionTest(); // ends the connection test
+    void continueSession(); // continues the session (maybe have a countdown for the duration?)
+    void updateIntensity(); // updates the currentIntensity variable according to user input
+    void savePreferences(); // saves the user's preferred intensity (where?)
+    void promptToRecord(); // post session, prompts user if they wish to record the just completed session
+    void recordTherapy(); // records the therapy session information (duration, frequency, type)
+    void createUser(); // handles the new user profile creation
+    void selectUser(); // handles the case where the user selects an existing profile
+    void signIn(); // handles user sign-in
+
+
     //changes the color of the light, can be private later or just cut&paste into different function.
     void greenLightOn(); //turns greenlight on
     void greenLightOff(); //turns the greenlight off
