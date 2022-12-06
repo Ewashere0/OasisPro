f2c09c6844c49a3d197e7cc5d22942e388c9357a
HenryVo <trungsonvo2001@gmail.com>
2022-12-06 14:42:30 -0500

    Added functionalities to cycle through session groups and sessions

--- Session.h
+++ Session.h
@@ -9,7 +9,7 @@ class Session {
     private:
         int id; // Unique identifier for each Session
         string frequencyRange; // Session Frequency Range (Page 12 Manual) - 4 available based on specs
-        string cesMode; // CES Mode (page 11 Manual) - 4 available
+        string cesMode; // CES Mode (page 11 Manual) - 2 available
 
     public:
         //Session(int n, string fr, string cm, int d); // Constructor to create a Session object (include all 4 member variables)
